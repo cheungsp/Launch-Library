fe599a3cd8f8dfdd39e5fb114c3c248ac7945c5e
Spencer Cheung <spencercheung1@gmail.com>
2017-09-27 01:09:15 -0700

    comments

--- src/index.js
+++ src/index.js
@@ -1,16 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import ReactDOM, { render } from 'react-dom';
-// import { Provider } from 'react-redux';
-// import { createStore, applyMiddleware } from 'redux';
-
 import App from './components/App/index';
-// import reducers from './reducers';
-
-// const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
-  // <Provider store={createStoreWithMiddleware(reducers)}>
     <App />
-  // </Provider>
   , document.querySelector('#root'));
